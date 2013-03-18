1ae0d9ffb50724eb067766dc0a01ec01cf4e20a5
Technoboy10 <technoboy10@fake.email>
2013-03-18 11:12:42 -0600

    Initial commit

---
+++ snap-server.py
@@ -0,0 +1,39 @@
+#Snap! extension base by Technoboy10
+import SimpleHTTPServer
+class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def send_head(self):
+	path = self.path
+	print path
+	ospath = os.path.abspath('')
+	if 'command' in path:
+		regex = re.compile("\/command(othercommands)(in|regex)")
+		m = regex.match(path)
+		#do commands
+	elif path == '/nxttouch': #return data
+		f = open(ospath + '/return', 'w+')
+		f.write("your data")
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+	        return f
+if __name__ == "__main__":
+    import your_modules
+    
+    PORT = 1337 #Pick a port number
+
+    Handler = CORSHTTPRequestHandler
+    #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+    print "serving at port", PORT
+    print "Go ahead and launch Snap!."
+    httpd.serve_forever()
+
